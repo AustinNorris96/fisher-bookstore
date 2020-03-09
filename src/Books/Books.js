0d86b83317884c2a5d91087e2838d245aa479279
Austin Norris <norris.701@osu.edu>
2020-03-08 21:32:57 -0500

    Send It

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Books.css";
+import { BookDisplay } from "./BookDisplay";
 
-export default function Books() {
+export default class Books extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id: 1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "987-03211125217"
+                },
+                {
+                    id: 2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-194278831"
+                }
+            ]
+        };
+    }
+    
+render() {
     return (
         <div className="Books">
             <div className="lander">
-                <h1>Books</h1>
-                <p>Here are a list of books.</p>
+                <BookDisplay books={this.state.books} />
             </div>
         </div>
-    );
+        );
+    }
 }
