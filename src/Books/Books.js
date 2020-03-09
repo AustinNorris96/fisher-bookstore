9325ef3bbde5e0d174321c4985a2aac9034c60ed
Austin Norris <norris.701@osu.edu>
2020-03-08 22:06:00 -0500

    Authors

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -18,6 +18,42 @@ export default class Books extends Component {
                     title: "Accelerate",
                     author: "Nicole Forsgren",
                     isbn: "978-194278831"
+                },
+                {
+                    id: 3,
+                    title: "Harry Potter and the Philosopher's Stone",
+                    author: "J.K. Rowling",
+                    isbn: "987-03211125218"
+                },
+                {
+                    id: 4,
+                    title: "The Master and Margarita",
+                    author: "Mikhail Blgakov",
+                    isbn: "987-03211125219"
+                },
+                {
+                    id: 5,
+                    title: "Alice's Advenure in Wonderland",
+                    author: "Lewis Carroll",
+                    isbn: "987-03211125220"
+                },
+                {
+                    id: 6,
+                    title: "The Hobbit",
+                    author: "J. R. R. Tolkien",
+                    isbn: "987-03211125221"
+                },
+                {
+                    id: 7,
+                    title: "And Then There Were None",
+                    author: "Agatha Christie",
+                    isbn: "987-03211125222"
+                },
+                {
+                    id: 8,
+                    title: "Drea, of the Red Chamber",
+                    author: "Cha Xueqin",
+                    isbn: "987-03211125223"
                 }
             ]
         };
